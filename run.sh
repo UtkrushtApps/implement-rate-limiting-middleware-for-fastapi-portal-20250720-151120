887a83a883af35afaff4dbf3b5897f1e24af90f6
Naman Muley <naman.g.muley@gmail.com>
2025-07-20 15:11:47 +0530

    Add run script

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-set -e
-
-./install.sh
-
-sleep 2
-echo "[run.sh] Listing running containers:"
-docker ps
-
-echo "[run.sh] Backend service should now be accessible at http://localhost:8000"
+./run.sh
