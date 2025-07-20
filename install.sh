4771391ff67de9bb983346f97e82caa34c6059e8
Naman Muley <naman.g.muley@gmail.com>
2025-07-20 15:11:45 +0530

    Add installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-set -e
-
-echo "[install.sh] Building Docker images..."
-docker-compose build
-
-echo "[install.sh] Starting backend service..."
-docker-compose up -d
-
-echo "[install.sh] Environment setup complete."
+./install.sh
